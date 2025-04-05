cd76dc26faa4efca426c7014684a7b0e1efce500
muhamamdzeeshan2 <muhammadzeeshannkn@gmail.com>
2025-04-05 08:43:36 +0500

    Remove whisper files from tracking and ignore them

--- whisper-small.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor
-local_model_path = "./whisper-small-local"
-model = AutoModelForSpeechSeq2Seq.from_pretrained("openai/whisper-small")
-processor = AutoProcessor.from_pretrained("openai/whisper-small")
-
-# Save the model and processor locally
-model.save_pretrained(local_model_path)
-processor.save_pretrained(local_model_path)
